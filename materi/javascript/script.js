a47600c9c5ddd691787117cc6f921f31decb6d52
Purnawibawa1 <adiwibowo200026@gmail.com>
2020-04-02 09:51:23 +0800

    video_part_2

--- materi/javascript/script.js
+++ materi/javascript/script.js
@@ -1,11 +1,30 @@
 (function (){
     "use strict";
-    var my_function = function (event){
-    alert("hello mahasiswa primakara")
-    event.preventDefault();
-};
-    var form = document.getElementById("cart-hplus");
-    form.addEventListener("submit", my_function, true);
+    document.addEventListener("DOMContentLoaded", function (){
+        var state = document.getElementById("s-state");
 
+        document
+        .getElementById("cart-hplus")
+        addEventListener("submit", estimateTotal);
+
+        var btnEstimate = document.getElementById("btn-estimate");
+
+        btnEstimate.disabled = true;
+
+        state.addEventListener("change", function (){
+            if (state.value == ""){
+                btnEstimate.disabled = true;     
+
+            }else {
+                btnEstimate.disabled = false;
+            }
+        });
+
+        function estimateTotal(event){
+            event.preventDefault();
+            alert("testing");
+        }
+
+    });
 
 })();
